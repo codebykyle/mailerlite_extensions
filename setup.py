be570860dbebb1155c4c54b166367b22a23b8c33
CodeByKyle <kyle@codebykyle.com>
2024-02-11 17:11:11 -0500

    Update URL in python package

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setuptools.setup(name='mailerlite_extensions',
                  long_description=open('README.md').read().strip(),
                  author='Kyle Shovan',
                  author_email='kyle@codebykyle.com',
-                 url='http://path-to-my-packagename',
+                 url='https://github.com/codebykyle/mailerlite_extensions',
                  py_modules=['mailerlite_extensions'],
                  install_requires=[],
                  license='MIT License',
                  zip_safe=False,
-                 keywords='boilerplate package',
+                 keywords='mailerlite',
                  classifiers=[
                      'Packages',
-                     'Boilerplate'
+                     'Mailerlite'
                  ])
